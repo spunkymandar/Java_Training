1974c2116f323969949e644ba706594274a2f3de
mandar123 <mandarmjog@gmail.com>
2021-02-23 20:18:33 +0530

    day_1

---
+++ Day1/src/Second.java
@@ -0,0 +1,13 @@
+
+public class Second {
+	public static void main(String[] args) {
+		int a,b,c;
+		a=10;
+		b=20;
+		c=a+b;
+		//System.out.println("Addition ="+c);
+		
+		System.out.println("Addition of "+a+ " and "+b+" = "+c);
+	}
+
+}
